ed8b2f63a98f7d5a7093fca0c8cab8d1db5142fa
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 06:11:51 -0500

    Update api.php

--- api.php
+++ api.php
@@ -1,23 +1,31 @@
 <?php
-$host = 'a4.database.windows.net';
-$username = 'A4';
-$password = 'Test1234!';
-$db_name = 'a4';
+// Use environment variables or secure configuration for credentials
+$host = getenv('DB_HOST') ?: 'a4.database.windows.net';
+$username = getenv('DB_USER') ?: 'A4';
+$password = getenv('DB_PASS') ?: 'Test1234!';
+$db_name = getenv('DB_NAME') ?: 'a4';
 
-//Establishes the connection
+// Establish the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+if (!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-//Run the Select query
-printf("Reading data from table: \n");
-$res = mysqli_query($conn, 'SELECT * FROM Patients');
-while ($row = mysqli_fetch_assoc($res)) {
-var_dump($row);
+// Run the SELECT query
+echo "Reading data from table:\n";
+$query = 'SELECT * FROM Patients';
+$res = mysqli_query($conn, $query);
+
+if ($res) {
+    echo "<pre>";
+    while ($row = mysqli_fetch_assoc($res)) {
+        print_r($row);
+    }
+    echo "</pre>";
+} else {
+    die('Query failed: ' . mysqli_error($conn));
 }
 
-//Close the connection
+// Close the connection
 mysqli_close($conn);
 ?>
