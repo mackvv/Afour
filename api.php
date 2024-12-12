0a2168e1972a15a67df768100b337aedcec3d429
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 06:01:16 -0500

    Update api.php

--- api.php
+++ api.php
@@ -1,3 +1,29 @@
 <?php
-echo '<img src="https://img.freepik.com/premium-photo/cat-wearing-sunglasses-with-sun-shining-it_910054-8427.jpg?w=360" alt="Cat wearing sunglasses" style="max-width: 99%; height: auto;">';
+$host = 'a4.database.windows.net';
+$username = 'A4';
+$password = 'Test1234!';
+$db_name = 'a4';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Products (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
+
+//Close the connection
+mysqli_close($conn);
 ?>
