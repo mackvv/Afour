6124d49627b3f946ff412734490b5dd42beefff8
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 04:58:30 -0500

    Create api.php

---
+++ api.php
@@ -0,0 +1,16 @@
+
+<?php
+try {
+   $serverName = getenv('a4.database.windows.net');
+$database = getenv('Patients');
+$username = getenv('A4');
+$password = getenv('Test1234!');
+
+ // Attempt to connect
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo json_encode(["status" => "success", "message" => "Database connected"]);
+} catch (PDOException $e) {
+    echo json_encode(["status" => "error", "message" => $e->getMessage()]);
+}
+?>
