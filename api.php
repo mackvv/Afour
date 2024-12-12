3979669155b2e763d63ddba0260559c54da1a549
Mackenzie Van Vliet <mackenzievanvliet@gmail.com>
2024-12-12 06:01:43 -0500

    Update api.php

--- api.php
+++ api.php
@@ -11,18 +11,6 @@ if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-// Run the create table query
-if (mysqli_query($conn, '
-CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
-);
-')) {
-printf("Table created\n");
-}
 
 //Close the connection
 mysqli_close($conn);
